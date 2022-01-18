eadcb7ca56d28cd0f4104577645e35899cc4031a
Kevin Guerrero <guerrk83z@gmail.com>
2022-01-18 14:10:52 -0500

    commit tree 306d2b51b0c1523bba63914e721422dae9867df9

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user'
+class Student < User
+    attr_accessor :first_name, :last_name
+    def initialize first_name = nil, last_name = nil
+        @first_name = first_name
+        @last_name = last_name
+        @knowledge = []
+    end
+
+    def learn string
+        @knowledge.append(string)
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
